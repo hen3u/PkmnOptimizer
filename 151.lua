f4e3cbfbbc98d07e2545967566b0136352dfafb5
Loïc HEN <loic.hen@telecomnancy.net>
2024-05-02 21:42:51 +0200

    gen1: add first 151 pkmn with type

---
+++ 151.lua
@@ -0,0 +1,668 @@
+local bulbasaur = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local ivysaur = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local venusaur = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local charmander = {
+    type1 = "Fire",
+    type2 = nil
+}
+local charmeleon = {
+    type1 = "Fire",
+    type2 = nil
+}
+local charizard = {
+    type1 = "Fire",
+    type2 = "Flying"
+}
+local squirtle = {
+    type1 = "Water",
+    type2 = nil
+}
+local wartortle = {
+    type1 = "Water",
+    type2 = nil
+}
+local blastoise = {
+    type1 = "Water",
+    type2 = nil
+}
+local caterpie = {
+    type1 = "Bug",
+    type2 = nil
+}
+local metapod = {
+    type1 = "Bug",
+    type2 = nil
+}
+local butterfree = {
+    type1 = "Bug",
+    type2 = "Flying"
+}
+local weedle = {
+    type1 = "Bug",
+    type2 = "Poison"
+}
+local kakuna = {
+    type1 = "Bug",
+    type2 = "Poison"
+}
+local beedrill = {
+    type1 = "Bug",
+    type2 = "Poison"
+}
+local pidgey = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+local pidgeotto = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+local pidgeot = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+local rattata = {
+    type1 = "Normal",
+    type2 = nil
+}
+local raticate = {
+    type1 = "Normal",
+    type2 = nil
+}
+local spearow = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+local fearow = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+local ekans = {
+    type1 = "Poison",
+    type2 = nil
+}
+local arbok = {
+    type1 = "Poison",
+    type2 = nil
+}
+local pikachu = {
+    type1 = "Electric",
+    type2 = nil
+}
+local raichu = {
+    type1 = "Electric",
+    type2 = nil
+}
+local sandshrew = {
+    type1 = "Ground",
+    type2 = nil
+}
+local sandslash = {
+    type1 = "Ground",
+    type2 = nil
+}
+local nidoran_f = {
+    type1 = "Poison",
+    type2 = nil
+}
+local nidorina = {
+    type1 = "Poison",
+    type2 = nil
+}
+local nidoqueen = {
+    type1 = "Poison",
+    type2 = "Ground"
+}
+local nidoran_m = {
+    type1 = "Poison",
+    type2 = nil
+}
+local nidorino = {
+    type1 = "Poison",
+    type2 = nil
+}
+local nidoking = {
+    type1 = "Poison",
+    type2 = "Ground"
+}
+local clefairy = {
+    type1 = "Fairy",
+    type2 = nil
+}
+local clefable = {
+    type1 = "Fairy",
+    type2 = nil
+}
+local jigglypuff = {
+    type1 = "Normal",
+    type2 = "Fairy"
+}
+local wigglytuff = {
+    type1 = "Normal",
+    type2 = "Fairy"
+}
+local zubat = {
+    type1 = "Poison",
+    type2 = "Flying"
+}
+local golbat = {
+    type1 = "Poison",
+    type2 = "Flying"
+}
+local oddish = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local gloom = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local vileplume = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local paras = {
+    type1 = "Bug",
+    type2 = "Grass"
+}
+local parasect = {
+    type1 = "Bug",
+    type2 = "Grass"
+}
+local venonat = {
+    type1 = "Bug",
+    type2 = "Poison"
+}
+local venomoth = {
+    type1 = "Bug",
+    type2 = "Poison"
+}
+local diglett = {
+    type1 = "Ground",
+    type2 = nil
+}
+local dugtrio = {
+    type1 = "Ground",
+    type2 = nil
+}
+local meowth = {
+    type1 = "Normal",
+    type2 = nil
+}
+local persian = {
+    type1 = "Normal",
+    type2 = nil
+}
+local psyduck = {
+    type1 = "Water",
+    type2 = nil
+}
+local golduck = {
+    type1 = "Water",
+    type2 = nil
+}
+local mankey = {
+    type1 = "Fighting",
+    type2 = nil
+}
+local primeape = {
+    type1 = "Fighting",
+    type2 = nil
+}
+local growlithe = {
+    type1 = "Fire",
+    type2 = nil
+}
+local arcanine = {
+    type1 = "Fire",
+    type2 = nil
+}
+local poliwag = {
+    type1 = "Water",
+    type2 = nil
+}
+local poliwhirl = {
+    type1 = "Water",
+    type2 = nil
+}
+local poliwrath = {
+    type1 = "Water",
+    type2 = "Fighting"
+}
+local abra = {
+    type1 = "Psychic",
+    type2 = nil
+}
+local kadabra = {
+    type1 = "Psychic",
+    type2 = nil
+}
+local alakazam = {
+    type1 = "Psychic",
+    type2 = nil
+}
+local machop = {
+    type1 = "Fighting",
+    type2 = nil
+}
+local machoke = {
+    type1 = "Fighting",
+    type2 = nil
+}
+local machamp = {
+    type1 = "Fighting",
+    type2 = nil
+}
+local bellsprout = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local weepinbell = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local victreebel = {
+    type1 = "Grass",
+    type2 = "Poison"
+}
+local tentacool = {
+    type1 = "Water",
+    type2 = "Poison"
+}
+local tentacruel = {
+    type1 = "Water",
+    type2 = "Poison"
+}
+local geodude = {
+    type1 = "Rock",
+    type2 = "Ground"
+}
+local graveler = {
+    type1 = "Rock",
+    type2 = "Ground"
+}
+local golem = {
+    type1 = "Rock",
+    type2 = "Ground"
+}
+local ponyta = {
+    type1 = "Fire",
+    type2 = nil
+}
+local rapidash = {
+    type1 = "Fire",
+    type2 = nil
+}
+local slowpoke = {
+    type1 = "Water",
+    type2 = "Psychic"
+}
+
+local slowbro = {
+    type1 = "Water",
+    type2 = "Psychic"
+}
+
+local magnemite = {
+    type1 = "Electric",
+    type2 = "Steel"
+}
+
+local magneton = {
+    type1 = "Electric",
+    type2 = "Steel"
+}
+
+local farfetchd = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+
+local doduo = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+
+local dodrio = {
+    type1 = "Normal",
+    type2 = "Flying"
+}
+
+local seel = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local dewgong = {
+    type1 = "Water",
+    type2 = "Ice"
+}
+
+local grimer = {
+    type1 = "Poison",
+    type2 = nil
+}
+
+local muk = {
+    type1 = "Poison",
+    type2 = nil
+}
+
+local shellder = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local cloyster = {
+    type1 = "Water",
+    type2 = "Ice"
+}
+
+local gastly = {
+    type1 = "Ghost",
+    type2 = "Poison"
+}
+
+local haunter = {
+    type1 = "Ghost",
+    type2 = "Poison"
+}
+
+local gengar = {
+    type1 = "Ghost",
+    type2 = "Poison"
+}
+
+local onix = {
+    type1 = "Rock",
+    type2 = "Ground"
+}
+
+local drowzee = {
+    type1 = "Psychic",
+    type2 = nil
+}
+
+local hypno = {
+    type1 = "Psychic",
+    type2 = nil
+}
+
+local krabby = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local kingler = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local voltorb = {
+    type1 = "Electric",
+    type2 = nil
+}
+
+local electrode = {
+    type1 = "Electric",
+    type2 = nil
+}
+
+local exeggcute = {
+    type1 = "Grass",
+    type2 = "Psychic"
+}
+
+local exeggutor = {
+    type1 = "Grass",
+    type2 = "Psychic"
+}
+
+local cubone = {
+    type1 = "Ground",
+    type2 = nil
+}
+
+local marowak = {
+    type1 = "Ground",
+    type2 = nil
+}
+
+local hitmonlee = {
+    type1 = "Fighting",
+    type2 = nil
+}
+
+local hitmonchan = {
+    type1 = "Fighting",
+    type2 = nil
+}
+
+local lickitung = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local koffing = {
+    type1 = "Poison",
+    type2 = nil
+}
+
+local weezing = {
+    type1 = "Poison",
+    type2 = nil
+}
+
+local rhyhorn = {
+    type1 = "Ground",
+    type2 = "Rock"
+}
+
+local rhydon = {
+    type1 = "Ground",
+    type2 = "Rock"
+}
+
+local chansey = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local tangela = {
+    type1 = "Grass",
+    type2 = nil
+}
+
+local kangaskhan = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local horsea = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local seadra = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local goldeen = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local seaking = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local staryu = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local starmie = {
+    type1 = "Water",
+    type2 = "Psychic"
+}
+
+local mr_mime = {
+    type1 = "Psychic",
+    type2 = "Fairy"
+}
+
+local scyther = {
+    type1 = "Bug",
+    type2 = "Flying"
+}
+
+local jynx = {
+    type1 = "Ice",
+    type2 = "Psychic"
+}
+
+local electabuzz = {
+    type1 = "Electric",
+    type2 = nil
+}
+
+local magmar = {
+    type1 = "Fire",
+    type2 = nil
+}
+
+local pinsir = {
+    type1 = "Bug",
+    type2 = nil
+}
+
+local tauros = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local magikarp = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local gyarados = {
+    type1 = "Water",
+    type2 = "Flying"
+}
+
+local lapras = {
+    type1 = "Water",
+    type2 = "Ice"
+}
+
+local ditto = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local eevee = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local vaporeon = {
+    type1 = "Water",
+    type2 = nil
+}
+
+local jolteon = {
+    type1 = "Electric",
+    type2 = nil
+}
+
+local flareon = {
+    type1 = "Fire",
+    type2 = nil
+}
+
+local porygon = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local omanyte = {
+    type1 = "Rock",
+    type2 = "Water"
+}
+
+local omastar = {
+    type1 = "Rock",
+    type2 = "Water"
+}
+
+local kabuto = {
+    type1 = "Rock",
+    type2 = "Water"
+}
+
+local kabutops = {
+    type1 = "Rock",
+    type2 = "Water"
+}
+
+local aerodactyl = {
+    type1 = "Rock",
+    type2 = "Flying"
+}
+
+local snorlax = {
+    type1 = "Normal",
+    type2 = nil
+}
+
+local articuno = {
+    type1 = "Ice",
+    type2 = "Flying"
+}
+
+local zapdos = {
+    type1 = "Electric",
+    type2 = "Flying"
+}
+
+local moltres = {
+    type1 = "Fire",
+    type2 = "Flying"
+}
+
+local dratini = {
+    type1 = "Dragon",
+    type2 = nil
+}
+
+local dragonair = {
+    type1 = "Dragon",
+    type2 = nil
+}
+
+local dragonite = {
+    type1 = "Dragon",
+    type2 = "Flying"
+}
+
+local mewtwo = {
+    type1 = "Psychic",
+    type2 = nil
+}
+
+local mew = {
+    type1 = "Psychic",
+    type2 = nil
+}
